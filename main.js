c910895c1d1fba5abdb4fc89483628199bea9cf8
Lars Weimar <Eisbaeeer@gmail.com>
2018-07-11 14:13:35 +0200

    dev

--- main.js
+++ main.js
@@ -115,7 +115,7 @@ adapter.on('stateChange', function (id, state) {
 		//var xml-construct = ;
 		
 			// Post DATA to DEVICE
-			var data = '<?xml version="1.0" encoding="UTF-8"?><Devices><Device><ID>'+ device_id +'</ID><HEATAREA nr="'+ heatarea +'"><'+ new_target +'>'+ new_val +'</'+ new_target +'></HEATAREA>'</Device></Devices>';
+			var data = '<?xml version="1.0" encoding="UTF-8"?><Devices><Device><ID>'+ device_id +'</ID><HEATAREA nr="'+ heatarea +'"><'+ new_target +'>'+ new_val +'</'+ new_target +'></HEATAREA></Device></Devices>';
 			httpPost(data);
 	}
 });
