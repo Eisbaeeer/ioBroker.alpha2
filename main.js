da300d3887840bed51303f8737373da17fa5a905
Lars Weimar <Eisbaeeer@gmail.com>
2018-07-12 10:13:46 +0200

    adminpolltime

--- main.js
+++ main.js
@@ -326,9 +326,9 @@ function main() {
 	* example 
 	* setInterval(pifaceread, adapter.config.piinterval);
 	*/
-	//setInterval(getXMLcyclic, parseInt(adapter.config.pollime));
-	adapter.log.info('admin polltime: ' + adapter.config.myText);
-	setInterval(getXMLcyclic, 30000);
+	setInterval(getXMLcyclic, adapter.config.myText);
+	adapter.log.info('config admin polltime: ' + adapter.config.myText);
+	//setInterval(getXMLcyclic, 30000);
 
 
     /**
